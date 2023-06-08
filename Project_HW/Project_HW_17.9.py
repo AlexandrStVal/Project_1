ad4d30724d36747193ccda7c3daa0e7e5503ed59
AlexandrStV <starikovichalexandr@gmail.com>
2023-06-09 02:07:41 +0700

    HW modul 17.9 correct duplication

--- Project_HW/Project_HW_17.9.py
+++ Project_HW/Project_HW_17.9.py
@@ -11,7 +11,7 @@ def list_sort(nums):
         nums[item], nums[min_index] = nums[min_index], nums[item]
 # запуск функции
 list_sort(list_of_numbers)
-print(set(list_of_numbers))
+print(list_of_numbers)
 print('_______')
 
 # # Двоичный поиск требует сортировки элементов в списке
